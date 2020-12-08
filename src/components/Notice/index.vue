3a8ea9237b878a2f0a761a9a61ef5b8e02de4c78
wh-lv <palomawhl@163.com>
2020-12-08 19:40:44 +0800

    '弹窗类型组件'

---
+++ src/components/Notice/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    message: {
+      type: String,
+      default: ''
+    },
+    duration: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
